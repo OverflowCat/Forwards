ef959e42b7b74c961c37056d97a597be47ba9f9b
Glitch (forwords) <none>
2019-08-29 20:08:04 +0000

    🦄🏌 Deleted stupid src urls

--- dict.js
+++ dict.js
@@ -1,37 +1,45 @@
 const CRAWLER = require("crawler");
+
 Array.prototype.insert = function (index, item) {
-  this.splice(index, 0, item);
-  return this
-};
+  return this.splice(index, 0, item)
+}
 var c = new CRAWLER({
-    maxConnections : 5,
-    // This will be called for each crawled page
+  maxConnections: 5,
+  // This will be called for each crawled page
 })
-function iciba(ctx){
+
+function iciba(ctx) {
   var w = ctx.message.text
   c.queue({
-    uri: "http://dict.youdao.com/m/search?keyfrom=dict.mresult&q="  + w
-    ,callback: function (error, res, done) {
-        if(error){
-            console.log(error)
-        }else{
-          var $ = res.$
-          var listtrans = $("div.content#listtrans ul")
-          var netexplanations = listtrans.slice(0,1)
-          var netex = netexplanations.text().replace(/\s/g, ' ')
-          var eg = listtrans.slice(1).text().split("\n").map(i=> {
-            i.trim()
-         const srcURLre = "/[-A-Za-z0-9+:,.;]+[A-Za-z]$/"
+    uri: "http://dict.youdao.com/m/search?keyfrom=dict.mresult&q=" + w,
+    callback: function (error, res, done) {
+      if (error) {
+        console.log(error)
+      } else {
+        var $ = res.$
+        var listtrans = $("div.content#listtrans ul")
+        var netexplanations = listtrans.slice(0, 1)
+        var netex = netexplanations.text().replace(/\s/g, ' ').trim()
+        var eg = listtrans.slice(1).text().split("\n").map(i => {
+          i = i.trim()
+          const srcURLre = "/[-A-Za-z0-9+:,.;]+[A-Za-z]$/"
           i = i.replace(srcURLre, '')
-          }).join('\n').trim()
-          var explanations = $("div.content").slice(0, 1).html().split(' ').join('').split("\n").map(i=>i.trim()).join('').split("<br>").map(i=>i.trim().split(".").insert(1, " ").join("")).join('\n')
-          var ex = unescape(explanations.replace(/&#x/g,'%u').replace(/;/g,'')) 
-          console.log(ex,eg, listtrans.slice(1).text())
-          var reply =  ex + netex + eg
-          ctx.reply(reply)
-        }
-        done();
+          var index = i.lastIndexOf("。")
+          return index >= 0 ? i.substring(0, index ) : i
+        }).join('\n').trim()
+        var explanations = $("div.content").slice(0, 1).html().split(' ').join('').split("\n").map(i => i.trim()).join('').split("<br>").map(i => i.trim().split(".").insert(1, " ").join("")).join('\n')
+        var ex = unescape(explanations.replace(/&#x/g, '%u').replace(/;/g, '')).trim()
+        //console.log(ex,eg, listtrans.slice(1).text())
+        var reply = ex + "\n网络释义\n" + netex + "\n例句\n" + eg
+        const extra = require('telegraf/extra')
+        const md = extra.markdown()
+
+        ctx.reply(reply)
+      }
+      done();
     }
-});
+  });
 }
-module.exports = {iciba}
+module.exports = {
+  iciba
+}
